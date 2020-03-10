34ee822ec165f342964a1bee8ae2640b6fb0ca51
davidmlee4 <davidmlee4@gmail.com>
2020-03-10 17:37:33 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
-document.addEventListener("DOMContentLoaded", () => {
+const ul = document.querySelector("ul")
+
+document.addEventListener("DOMContentLoaded", (event) => {
   // your code here
+  document.addEventListener('submit', function(event){
+    event.preventDefault()
+    const input = document.getElementById("new-task-description")
+    const li = document.createElement("li")
+    li.className = "tasks"
+    li.innerHTML = `
+    <h3>${input.value} <button>&times;</button></h3>`
+
+    ul.append(li)
+  })
+
+  document.addEventListener('click',function(event){
+    
+  })
+
 });
+
+
